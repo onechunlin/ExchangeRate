c562b46e14c4f6ccbcb2d525610a43a0237980c5
onechunlin <865106110@qq.com>
2020-05-08 12:33:23 +0800

    Update script.js

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function calculate(){
         .then(data => {
             const rate = data.rates[currency_two];
             
-            rateEl.innerText = `1 ${currency_one} = ${rate} ${currency_two}`;
+            rateEl.innerText = `1 ${currency_one} = ${rate.toFixed(4)} ${currency_two}`;
             amountEl_two.value = (amountEl_one.value * rate).toFixed(2);
         });
 }
